c5b089688beabdede1619b5de3cd2710d4b9467f
pyc <pyc.xuu@gmail.com>
2021-03-29 21:18:42 +1100

    first commit

---
+++ W2/FirstProgram.java
@@ -0,0 +1,26 @@
+/* 
+   The University of Melbourne
+   School of Computing and Information Systems
+   COMP90041 Programming and Software Development
+   Lecturers: Dr Udaya Parampalli and Dr Thuan Pham
+   Semester 1, 2021, Week 2
+   Workshop Sample Program
+   Copyright The University of Melbourne 2021
+   
+   A simple hello world program
+   Jianzhong Qi, 10 February 2016
+*/
+
+public class FirstProgram {
+	public static void main(String[] args) {
+		System.out.println("Hello reader.");
+		System.out.println("Welcome to Java.");
+
+		System.out.println("Let's demonstrate a simple calculation.");
+		int answer;
+		answer = 2 + 2;
+		System.out.println("2 plus 2 is " + answer);
+      String msg = "Hello, World";
+      System.out.println(msg.toUpperCase());
+	}
+}
